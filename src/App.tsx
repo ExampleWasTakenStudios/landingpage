38115cbc3496f8d0bd927e640f1f10e4e17e5522
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-02-16 14:49:17 +0100

    chore: remove bg image (#29)
    * remove image from landingpage
    
    * delete profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Clock } from './clock/Clock';
 export const App = () => {
   return (
     <>
-      <div className="absolute -z-50 w-screen h-screen bg-purple bg-gradient-to-tr from-purple to-orange">
-        <div className="bg-profilePicture w-full h-full bg-no-repeat bg-contain bg-[-30px] sm:bg-center blur-[100px]"></div>
-      </div>
+      <div className="absolute -z-50 w-screen h-screen bg-purple bg-gradient-to-tr from-purple to-orange"></div>
       <main className="w-screen h-screen flex flex-col justify-evenly items-center">
         <h1 className="text-white text-3xl tracking-widest font-thin drop-shadow-clockHand">COMING</h1>
         <Clock />
