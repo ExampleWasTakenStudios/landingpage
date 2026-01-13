17ad7107800e9cef2736f02bb53874cd3b019f91
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-01-13 22:50:39 +0100

    feat: add animated clock (#38)
    * add animated clock
    
    * fix prettier and lint
    
    * add clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Clock } from './clock/Clock';
+import AnimatedClock from './clock/Clock';
 
 export const App = () => {
   return (
     <>
       <div className="absolute -z-50 w-screen h-screen bg-purple bg-gradient-to-tr from-purple to-orange"></div>
       <main className="w-screen h-screen flex flex-col justify-evenly items-center">
-        <h1 className="text-white text-3xl tracking-widest font-thin drop-shadow-clockHand">COMING</h1>
-        <Clock />
-        <h1 className="text-white text-3xl tracking-widest font-thin drop-shadow-clockHand">SOON</h1>
+        <h1 className="text-white bg-transparent text-3xl tracking-widest font-thin">COMING</h1>
+        <AnimatedClock />
+        <h1 className="text-white bg-transparent text-3xl tracking-widest font-thin shadow-2xl">SOON</h1>
       </main>
     </>
   );
--- src/clock/HourHand.tsx
+++
@@ -1,5 +0,0 @@
-export const HourHand = () => {
-  return (
-    <div className="drop-shadow-clockHand w-[4px] h-[110px] rounded-full bg-white absolute left-[150px] top-[96px] -translate-x-1/2 -translate-y-1/2 origin-bottom rotate-[60deg]"></div>
-  );
-};
